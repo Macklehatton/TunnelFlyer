305bf13573d7dc5dd8783b093bbb4538225fa582
Macklehatton <macklehatton@gmail.com>
2014-08-08 17:18:04 -0500

    Revert

--- Assets/Scripts/CopyRotation.cs
+++ Assets/Scripts/CopyRotation.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class CopyRotation : MonoBehaviour {
 
 	public GameObject target;
-	public float speed;	
+	public float speed;
+	public Vector3 targetRotation;
 
-	void Update () {	
-				
+	void Update () {
 		transform.rotation = Quaternion.Lerp(transform.rotation, 
 		                                     target.transform.rotation,  
+<<<<<<< HEAD
 		                                     Mathf.SmoothStep(0.0f, 1.0f, speed));                                    
 		                                    
+=======
+		                                     Mathf.SmoothStep(0.0f, 1.0f, speed));		
+>>>>>>> parent of 4ab13f2... Rotation valleys and horizontal snapping
 	}
 }
--- Assets/Scripts/ModifiedSimpleMouseRotator.cs
+++ Assets/Scripts/ModifiedSimpleMouseRotator.cs
@@ -1,112 +1,133 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class ModifiedSimpleMouseRotator : MonoBehaviour {
 	
-	// Modification of the sample assets mouse rotator. Has valleys in rotation speed that are like a soft version of angle snaps
-	// Automatically levels out and snaps to a different set of angles when there is no input.
+	// A mouselook behaviour with constraints which operate relative to
+	// this gameobject's initial rotation.
+	
+	// Only rotates around local X and Y.
+	
+	// Works in local coordinates, so if this object is parented
+	// to another moving gameobject, its local constraints will
+	// operate correctly
+	// (Think: looking out the side window of a car, or a gun turret
+	// on a moving spaceship with a limited angular range)
+	
+	// to have no constraints on an axis, set the rotationRange to 360 or greater.
 
 	public Vector2 rotationRange = new Vector3(70,70); 
 	public float rotationSpeed = 10;
 	public float dampingTime = 0.2f;
 	public bool autoZeroVerticalOnMobile = true;
 	public bool autoZeroHorizontalOnMobile = false;
-	public Vector3 targetAngles;
-	public Vector3 followAngles;
-	public Vector3 followVelocity;
+	public bool relative = true;
+	Vector3 targetAngles;
+	Vector3 followAngles;
+	Vector3 followVelocity;
 	Quaternion originalRotation;
 
 	public bool leveling;
-	public float levelingTime;
+	public float levelingSpeed;
 	public float levelingTimer;
 	public float timeSinceInput;
 	public Quaternion lastRotation;
 	public Vector3 lastHeading;
-	public float headingChange;
-	public float fromNeutral;
-	
-	public bool snapping;
-	public Vector3 startRotation;
-	public Vector3 startAngles;
-	public float startTime;
-	public Quaternion zeroX;	
-	public float valleySnap;
-	public float settleSnap;
-	public float xDiff;
-	public float yDiff;
-	public float xAdjustment;
-	public float yAdjustment;
-	public float valleyDepth;
+	public float angle;
 
+
+	// Use this for initialization
 	void Start () {
 		originalRotation = transform.localRotation;
 	}
 	
-	void LateUpdate () {		
+	// Update is called once per frame
+	void Update () {
 
-		headingChange = Vector3.Angle (transform.forward, lastHeading);
-		fromNeutral = Quaternion.Angle (transform.localRotation, Quaternion.Euler (new Vector3 (0f, 0f, transform.localRotation.eulerAngles.z)));
+		angle = Vector3.Angle (transform.forward, lastHeading);
+
+		if (angle <= 0.0f) {
+			timeSinceInput += Time.deltaTime;
+		}
 
 		lastHeading = transform.forward;
 
-		if (headingChange <= 0.0f) {
-			timeSinceInput += Time.deltaTime;
-		} else {
-			timeSinceInput = 0.0f;
-		}
-		
 		if (timeSinceInput >= levelingTimer) {
-			LevelingOn();
+			leveling = true;
 		}
 
 		if (leveling) {
-			if (fromNeutral <= 0f) {
-				LevelingOff();
+			if (Quaternion.Angle(transform.localRotation, Quaternion.Euler(new Vector3(0f, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z))) <= 0f) {
+				leveling = false;
+				timeSinceInput = 0f;
 			}
 		}
 
+		// we make initial calculations from the original local rotation
 		if (!leveling) {
-			transform.localRotation = originalRotation;	
+			transform.localRotation = originalRotation;
 		}
 
+
+
+		// read input from mouse or mobile controls
 		float inputH = 0;
 		float inputV = 0;
+		if (relative)
+		{
+			#if CROSS_PLATFORM_INPUT
+			inputH = CrossPlatformInput.GetAxis("Mouse X");
+			inputV = CrossPlatformInput.GetAxis("Mouse Y");
+			#else
+			inputH = Input.GetAxis("Mouse X");
+			inputV = Input.GetAxis("Mouse Y");
+			#endif
+			// wrap values to avoid springing quickly the wrong way from positive to negative
+			if (targetAngles.y > 180) { targetAngles.y -= 360; followAngles.y -= 360; }
+			if (targetAngles.x > 180) { targetAngles.x -= 360; followAngles.x-= 360; }
+			if (targetAngles.y < -180) { targetAngles.y += 360; followAngles.y += 360; }
+			if (targetAngles.x < -180) { targetAngles.x += 360; followAngles.x += 360; }
 
-		inputH = CrossPlatformInput.GetAxis("Mouse X");
-		inputV = CrossPlatformInput.GetAxis("Mouse Y");
+			#if MOBILE_INPUT
+			// on mobile, sometimes we want input mapped directly to tilt value,
+			// so it springs back automatically when the look input is released.
+			if (autoZeroHorizontalOnMobile) {
+				targetAngles.y = Mathf.Lerp (-rotationRange.y * 0.5f, rotationRange.y * 0.5f, inputH * .5f + .5f);
+			} else {
+				if (leveling) {
+					targetAngles.y = 0.0f;
+				} else {
+					targetAngles.y += inputH * rotationSpeed;
+				}
+			}
+			if (autoZeroVerticalOnMobile) {
+				targetAngles.x = Mathf.Lerp (-rotationRange.x * 0.5f, rotationRange.x * 0.5f, inputV * .5f + .5f);
+			} else {
+				targetAngles.x += inputV * rotationSpeed;
+			}			
+			#else
+			// with mouse input, we have direct control with no springback required.
+			targetAngles.y += inputH * rotationSpeed;
+			targetAngles.x += inputV * rotationSpeed;
+			#endif
 
-		if (targetAngles.y > 180) { targetAngles.y -= 360; followAngles.y -= 360; }
-		if (targetAngles.x > 180) { targetAngles.x -= 360; followAngles.x-= 360; }
-		if (targetAngles.y < -180) { targetAngles.y += 360; followAngles.y += 360; }
-		if (targetAngles.x < -180) { targetAngles.x += 360; followAngles.x += 360; }		
-		
-		targetAngles.y += inputH * (rotationSpeed - yAdjustment);
-		targetAngles.x += inputV * (rotationSpeed - xAdjustment);								
+			// clamp values to allowed range
+			targetAngles.y = Mathf.Clamp ( targetAngles.y, -rotationRange.y * 0.5f, rotationRange.y * 0.5f );
+			targetAngles.x = Mathf.Clamp ( targetAngles.x, -rotationRange.x * 0.5f, rotationRange.x * 0.5f );
 
-		targetAngles.y = Mathf.Clamp ( targetAngles.y, -rotationRange.y * 0.5f, rotationRange.y * 0.5f );
-		targetAngles.x = Mathf.Clamp ( targetAngles.x, -rotationRange.x * 0.5f, rotationRange.x * 0.5f );			
-		
-		if (Mathf.Abs (inputH) > 0f || Mathf.Abs (inputH) > 0f) {
-			iTween.StopByName("RotateTo");
-			LevelingOff();			
-		}	
-
-		if (leveling) {
-			zeroX = transform.localRotation;
-			zeroX.eulerAngles = new Vector3 (0f, zeroX.eulerAngles.y, zeroX.eulerAngles.z);			
-			float ySettle = Mathf.Round ( transform.localRotation.eulerAngles.y / settleSnap) * settleSnap;
-			
-			iTween.RotateTo(gameObject, iTween.Hash("easetype", "easeInQuad", "time",levelingTime, "x",0f,"y", ySettle, "name", "RotateTo"));			
-			
 		} else {
-			followAngles = Vector3.SmoothDamp( followAngles, targetAngles, ref followVelocity, dampingTime );			
 
-			transform.localRotation = originalRotation * Quaternion.Euler( -followAngles.x, followAngles.y, 0 );
+			inputH = Input.mousePosition.x;
+			inputV = Input.mousePosition.y;
+
+			targetAngles.y = Mathf.Lerp ( -rotationRange.y * 0.5f, rotationRange.y * 0.5f, inputH/Screen.width );
+			targetAngles.x = Mathf.Lerp ( -rotationRange.x * 0.5f, rotationRange.x * 0.5f, inputV/Screen.height );
+
 		}
-		
-		xDiff = Mathf.Abs((Mathf.Round ( transform.localRotation.eulerAngles.x / valleySnap) * valleySnap) - transform.localRotation.eulerAngles.x)/valleySnap;
-		yDiff = Mathf.Abs((Mathf.Round ( transform.localRotation.eulerAngles.y / valleySnap) * valleySnap) - transform.localRotation.eulerAngles.y)/valleySnap;		
 
+		followAngles = Vector3.SmoothDamp( followAngles, targetAngles, ref followVelocity, dampingTime );
+		
+
+<<<<<<< HEAD
 		xAdjustment = Mathf.Pow ((1 - xDiff), 3 ) * valleyDepth * rotationSpeed;
 		yAdjustment = Mathf.Pow ((1 - yDiff), 3 ) * valleyDepth * rotationSpeed;
 		
@@ -125,14 +146,14 @@ public class ModifiedSimpleMouseRotator : MonoBehaviour {
 			leveling = true;
 		}
 	}
+=======
+		if (leveling) {			
+			transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.identity, Time.deltaTime * levelingSpeed);
+>>>>>>> parent of 4ab13f2... Rotation valleys and horizontal snapping
 
-	void LevelingOff() {
-		if (leveling) {	
-			Debug.Log ("Leveling off");
-			startAngles = Vector3.zero;
-			targetAngles = transform.localRotation.eulerAngles;
-			timeSinceInput = 0.0f;
-			leveling = false;
 		}
+
+		transform.localRotation = originalRotation * Quaternion.Euler( -followAngles.x, followAngles.y, 0 );
+
 	}
 }
