54281cb16b236d6f227f5998e183caf135545fca
Macklehatton <macklehatton@gmail.com>
2014-09-25 16:28:36 -0700

    Basic movement working again
    Ready to implement physics...?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,28 +6,52 @@ public class PlayerController : MonoBehaviour
 {
 	public float leftH;
 	public float leftV;
-
+	
+	public Vector2 input;
+	
 	public Vector3 strafeMove;
 	public Vector3 desiredMove;
 
 	public Camera playerCam;
-
+	public GameObject playerRig;
+	
+	public float strafeBounds;
+	
 	public float dodgeSpeed;
 
 	public float followDist;
-
+	
 	void Update ()	{
+	
 		leftH = CrossPlatformInput.GetAxis ("Horizontal");
 		leftV = CrossPlatformInput.GetAxis ("Vertical");
-
-		strafeMove = new Vector3 (leftH * Screen.width / 4 + (Screen.width / 2), 
-		                          leftV * Screen.height / 4 + (Screen.height / 2) - Screen.height / 6, 
-		                          followDist);
-
- 		desiredMove = playerCam.ScreenToWorldPoint(strafeMove);
-
+		
+		input = new Vector2( leftH, leftV );
+		
+		
+		
+		desiredMove = transform.up * input.y * strafeBounds + transform.right * input.x * strafeBounds + playerRig.transform.position;
+		
+					
 		transform.position = Vector3.Lerp (transform.position, 
 		                                   desiredMove, 
-		                                   Mathf.SmoothStep(0.0f, 1.0f, dodgeSpeed));
+		                                   Mathf.SmoothStep(0.0f, 1.0f, dodgeSpeed));		
+
+//		transform.position = Vector3.Lerp (transform.position, 
+//		                                   desiredMove, 
+//		                                   Mathf.SmoothStep(0.0f, 1.0f, dodgeSpeed));
+	
+//		leftH = CrossPlatformInput.GetAxis ("Horizontal");
+//		leftV = CrossPlatformInput.GetAxis ("Vertical");
+//
+//		strafeMove = new Vector3 (leftH * Screen.width / 4 + (Screen.width / 2), 
+//		                          leftV * Screen.height / 4 + (Screen.height / 2) - Screen.height / 6, 
+//		                          followDist);
+//
+// 		desiredMove = playerCam.ScreenToWorldPoint(strafeMove);
+//
+//		transform.position = Vector3.Lerp (transform.position, 
+//		                                   desiredMove, 
+//		                                   Mathf.SmoothStep(0.0f, 1.0f, dodgeSpeed));
 	}
 }
