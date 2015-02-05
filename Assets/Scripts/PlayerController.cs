0455f5d5ecf583b3bfda33fd5b798c3010cc7de2
Macklehatton <macklehatton@gmail.com>
2015-02-04 17:04:10 -0800

    Conflict

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,10 @@ public class PlayerController : MonoBehaviour
 {
 	public float leftH;
 	public float leftV;
-	
-	public Vector2 input;
-	
+
 	public Vector3 strafeMove;
 	public Vector3 desiredMove;
+<<<<<<< HEAD
 	
 	public float speed;
 	public float dodgeSpeed;
@@ -41,5 +40,27 @@ public class PlayerController : MonoBehaviour
 		force = Vector3.ClampMagnitude(gain * error, maxForce);
 		rigidbody.AddForce(force);                                   		
 		
+=======
+
+	public Camera playerCam;
+
+	public float dodgeSpeed;
+
+	public float followDist;
+
+	void Update ()	{
+		leftH = CrossPlatformInput.GetAxis ("Horizontal");
+		leftV = CrossPlatformInput.GetAxis ("Vertical");
+
+		strafeMove = new Vector3 (leftH * Screen.width / 4 + (Screen.width / 2), 
+		                          leftV * Screen.height / 4 + (Screen.height / 2) - Screen.height / 6, 
+		                          followDist);
+
+ 		desiredMove = playerCam.ScreenToWorldPoint(strafeMove);
+
+		transform.position = Vector3.Lerp (transform.position, 
+		                                   desiredMove, 
+		                                   Mathf.SmoothStep(0.0f, 1.0f, dodgeSpeed));
+>>>>>>> parent of 54281cb... Basic movement working again
 	}
 }
